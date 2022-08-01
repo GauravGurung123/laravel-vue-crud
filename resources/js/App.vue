be19b58cc8f103f3f0b7e545dc5486efc58cc86a
Gaurav <gauravgurung067@gmail.com>
2022-08-01 18:01:58 +0545

    setup project

---
+++ resources/js/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="collapse navbar-collapse">
+                <div className="navbar-nav">
+                    <router-link to="/" className="nav-item nav-link">Products List</router-link>
+                    <router-link to="/create" className="nav-item nav-link">Create Product</router-link>
+                </div>
+            </div>
+        </nav>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
---
+++ resources/js/components/AllProduct.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "AllProduct.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
